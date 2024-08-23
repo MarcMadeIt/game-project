db980a0296d85f9ec7229478a3cbcde39aa27c1c
Marc Made It <spurdo.dk@gmail.com>
2024-08-23 09:30:52 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,18 +2,18 @@ const openModalBtn = document.getElementById('create-game');
 const inputBox = document.getElementById('input-name');
 const submitBtn = document.getElementById('reset-btn');
 const overlay = document.getElementById('overlay');
-const modal = document.getElementById('create-game');
+const modal = document.getElementById('create-name');
 
 
 openModalBtn.addEventListener('click', () => {
-    modal.style.display = 'block';
+    modal.style.display = 'flex';
     overlay.style.display = 'block';
-  });
+});
 
-  closeBtn.addEventListener('click', () => {
+closeBtn.addEventListener('click', () => {
     modal.style.display = 'none';
     overlay.style.display = 'none';
-  });
+});
 
 submitBtn.addEventListener('click', () => {
     localStorage.removeItem('score');
@@ -23,11 +23,11 @@ submitBtn.addEventListener('click', () => {
             name: inputValue,
             score: 0
         };
-      localStorage.setItem('playerData', json.stringify(playerData));
-      console.log('Stored string:', inputValue);
-      modal.style.display = 'none';
-      overlay.style.display = 'none';
+        localStorage.setItem('playerData', json.stringify(playerData));
+        console.log('Stored string:', inputValue);
+        modal.style.display = 'none';
+        overlay.style.display = 'none';
     } else {
-      alert('Please enter a string');
+        alert('Please enter a string');
     }
-  });
+});
