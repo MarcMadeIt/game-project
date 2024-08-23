4327dc0958967cad95fc753f7e808c0ce93fb082
Xy'Pex Zukira <Sebastian@birdie.codes>
2024-08-23 09:31:34 +0200

    update

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const inputBox = document.getElementById('input-name');
 const submitBtn = document.getElementById('reset-btn');
 const overlay = document.getElementById('overlay');
 const modal = document.getElementById('create-name');
-
+const closeBtn = document.getElementById('close-btn');
 
 openModalBtn.addEventListener('click', () => {
     modal.style.display = 'flex';
