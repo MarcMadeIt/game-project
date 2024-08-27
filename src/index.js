ae47c2b8debe488fed2d00312fc90d2a68fea600
Xy'Pex Zukira <Sebastian@birdie.codes>
2024-08-27 11:25:42 +0200

    update

--- src/index.js
+++ src/index.js
@@ -1,13 +1,26 @@
 const openModalBtn = document.getElementById('create-game');
+
 const inputBox = document.getElementById('input-name');
 const submitBtn = document.getElementById('reset-btn');
+
 const overlay = document.getElementById('overlay');
 const modal = document.getElementById('create-name');
+
 const closeBtn = document.getElementById('close-btn');
+
 const contiuneBtn = document.getElementById('prev-cont');
+
+const rulesBtn = document.getElementById('rules-game');
+const rulesModal = document.getElementById('rules-content');
+
+rulesBtn.addEventListener('click', () => {
+    rulesModal.style.display = 'flex';
+    overlay.style.display = 'block';
+});
 
 openModalBtn.addEventListener('click', () => {
     modal.style.display = 'flex';
+    rulesModal.style.display = 'none';
     overlay.style.display = 'block';
 });
 
