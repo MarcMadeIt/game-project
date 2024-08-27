f2f55f19ae9f348a54582cdfb425170fe53b8f0c
Xy'Pex Zukira <Sebastian@birdie.codes>
2024-08-27 11:33:49 +0200

    update

--- src/index.js
+++ src/index.js
@@ -27,6 +27,7 @@ openModalBtn.addEventListener('click', () => {
 closeBtn.addEventListener('click', () => {
     modal.style.display = 'none';
     overlay.style.display = 'none';
+    rulesModal.style.display = 'none';
 });
 
 submitBtn.addEventListener('click', () => {
